f1c0fcd69bce9d0c7882dff237fe7fbb95242989
Jason Shanks <jason.s@wellmark.com.au>
2024-05-25 01:41:56 +1000

    updates to: alacritty, neovim, tmux (continuing to attempt to solve scrolling issues)
    Alacritty: have scrolling behaviour how I want (buffer scrollback, not
    command history)
    nvim: add some autocommands that show mouse scrolling in Tmux when open,
    re-enabling when closed (WIP)
    - ZenMode plugin: hide gitsigns, tmux and alacritty chrome
    - add Twilight plugin for focus during markdown writing
    tmux: re-enabled mouse

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -16,3 +16,20 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.conceallevel = 0
   end,
 })
+
+-- Disables tmux buffer scrollback so the neovim session doesn't scroll out of the viewport
+vim.api.nvim_create_augroup("TmuxMouse", { clear = true })
+
+-- Disable tmux mouse mode when entering nvim
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = "TmuxMouse",
+  pattern = "*",
+  command = "silent !tmux setw -g mouse off; sleep 0; sleep 0.1",
+})
+
+-- Enable tmux mouse mode when leaving nvim
+vim.api.nvim_create_autocmd("VimLeave", {
+  group = "TmuxMouse",
+  pattern = "*",
+  command = "silent !tmux setw -g mouse on; sleep 0.1",
+})
