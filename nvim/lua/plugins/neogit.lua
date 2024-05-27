449ca5ccec8627c2fa2f4e5eb0ba827e5bd425e2
Jason Shanks <jason.s@wellmark.com.au>
2024-05-27 15:19:41 +1000

    neovim: - Neogit plugin: disabled commit confirmation

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -4,10 +4,9 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
       "nvim-telescope/telescope.nvim", -- optional
     },
     config = true,
+    disable_commit_confirmation = true,
   },
 }
