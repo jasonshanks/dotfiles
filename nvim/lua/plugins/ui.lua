5b9b29f1ce5ac01618b885ac62e1bddb18378dbc
Jason Shanks <jason.s@wellmark.com.au>
2025-01-16 02:23:23 +1100

    feat(nvim): remove nvim-notify in favour of Snacks Notifier

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -53,13 +53,6 @@ return {
     end,
   },
 
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 5000,
-    },
-  },
-
   -- animations
   {
     "echasnovski/mini.animate",
@@ -170,35 +163,35 @@ return {
   },
 
   -- filename
-  {
-    "b0o/incline.nvim",
-    event = "VeryLazy",
-    config = function()
-      local helpers = require("incline.helpers")
-      local devicons = require("nvim-web-devicons")
-      require("incline").setup({
-        window = {
-          padding = 0,
-          margin = { horizontal = 1, vertical = 1 },
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if filename == "" then
-            filename = "[No Name]"
-          end
-          local ft_icon, ft_color = devicons.get_icon_color(filename)
-          local modified = vim.bo[props.buf].modified
-          return {
-            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-            " ",
-            { filename, gui = modified and "bold,italic" or "bold" },
-            " ",
-            guibg = "#44406e",
-          }
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "b0o/incline.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     local helpers = require("incline.helpers")
+  --     local devicons = require("nvim-web-devicons")
+  --     require("incline").setup({
+  --       window = {
+  --         padding = 0,
+  --         margin = { horizontal = 1, vertical = 1 },
+  --       },
+  --       render = function(props)
+  --         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+  --         if filename == "" then
+  --           filename = "[No Name]"
+  --         end
+  --         local ft_icon, ft_color = devicons.get_icon_color(filename)
+  --         local modified = vim.bo[props.buf].modified
+  --         return {
+  --           ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+  --           " ",
+  --           { filename, gui = modified and "bold,italic" or "bold" },
+  --           " ",
+  --           guibg = "#44406e",
+  --         }
+  --       end,
+  --     })
+  --   end,
+  -- },
 
   {
     "folke/zen-mode.nvim",
