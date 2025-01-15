1eab617417b259175e1f7021dd0c3355a1130855
Jason Shanks <jason.s@wellmark.com.au>
2025-01-16 02:11:47 +1100

    feat(nvim): removed git plugin, using snacks git features now

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -129,17 +129,4 @@ return {
       },
     },
   },
-
-  {
-    "dinhhuy258/git.nvim",
-    event = "BufReadPre",
-    opts = {
-      keymaps = {
-        -- Open blame window
-        blame = "<Leader>gb",
-        -- Open file/folder in git repository
-        browse = "<Leader>go",
-      },
-    },
-  },
 }
