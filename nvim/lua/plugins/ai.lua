5ccf349e37bf1024c643fc26c66e14eb0dbf9482
Jason Shanks <jason.s@wellmark.com.au>
2025-08-05 15:03:04 +1000

    feat(nvim): fix Windsurf/Codeium from protesting that nvim.cmp is not loaded (using Blink instead)

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -6,7 +6,9 @@ return {
       "saghen/blink.cmp",
     },
     config = function()
-      require("codeium").setup({})
+      require("codeium").setup({
+        enable_cmp_source = false,
+      })
     end,
   },
 }
