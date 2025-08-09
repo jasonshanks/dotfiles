a82c695ba278a2ab61e6f0a70f2add4d648aeed5
Jason Shanks <jason.s@wellmark.com.au>
2025-08-09 10:41:18 +1000

    refactor(nvim): rename all plugins in categories. feat(nvim): add diffview.nvim

--- nvim/lua/plugins/editor.hardtime.lua
+++ nvim/lua/plugins/editor.hardtime.lua
@@ -1,6 +1,7 @@
- {
+return {
   {
     "m4xshen/hardtime.nvim",
+    enabled = false,
     event = "VeryLazy",
     opts = {
       disabled_filetypes = { "aerial-nav", "qf", "netrw", "NvimTree", "lazy", "mason", "neo-tree" },
