ba74bce82701006407459f363b05526e4c8b42fe
Jason Shanks <jason.s@wellmark.com.au>
2025-08-09 11:03:00 +1000

    feat(nvim): add keymaps for diffview open and close

--- nvim/lua/plugins/coding.diffview.lua
+++ nvim/lua/plugins/coding.diffview.lua
@@ -1,3 +1,7 @@
 return {
   "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+  },
 }
