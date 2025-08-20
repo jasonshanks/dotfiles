4dfe11f0fe394f688aa5494f2c68877cfa9d6a37
Jason Shanks <jason.s@wellmark.com.au>
2025-08-20 12:22:56 +1000

    feat(nvim): rework edgy configs away from default recommended snacks integration and just explicitly specify other plugin target configs. (needs revisiting but all works currently except Outline loses it's pane title)

--- nvim/lua/plugins/ui.edgy.lua
+++ nvim/lua/plugins/ui.edgy.lua
@@ -1,41 +1,36 @@
 return {
   {
     "folke/edgy.nvim",
-    opts = function(_, opts)
-      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
-        opts[pos] = opts[pos] or {}
-        table.insert(opts[pos], {
-          ft = "snacks_terminal",
-          size = { height = 0.4 },
-          title = "%{b:snacks_terminal.id}: %{b:term_title}",
-          filter = function(_buf, win)
-            return vim.w[win].snacks_win
-              and vim.w[win].snacks_win.position == pos
-              and vim.w[win].snacks_win.relative == "editor"
-              and not vim.w[win].trouble_preview
-          end,
-        })
-      end
-    end,
+    -- opts = function(_, opts)
+    --   for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+    --     opts[pos] = opts[pos] or {}
+    --     table.insert(opts[pos], {
+    --       ft = "snacks_terminal",
+    --       size = { height = 0.4 },
+    --       title = "%{b:snacks_terminal.id}: %{b:term_title}",
+    --       filter = function(_buf, win)
+    --         return vim.w[win].snacks_win
+    --           and vim.w[win].snacks_win.position == pos
+    --           and vim.w[win].snacks_win.relative == "editor"
+    --           and not vim.w[win].trouble_preview
+    --       end,
+    --     })
+    --   end
+    -- end,
     event = "VeryLazy",
     init = function()
       vim.opt.laststatus = 3
       vim.opt.splitkeep = "screen"
     end,
     opts = {
-      bottom = {
-        {
-          title = "Snacks Terminal",
-          ft = "snacks_terminal",
-          size = { height = 0.4 },
-        },
-      },
+      bottom = {},
       right = {
         {
           ft = "opencode",
           size = { width = 0.3 },
         },
       },
+      float = {},
     },
   },
 }
