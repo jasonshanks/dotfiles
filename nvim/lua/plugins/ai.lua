4b3041af66ce4ebc48e7b42064747f63c23a35a8
Jason Shanks <jason.s@wellmark.com.au>
2026-04-09 11:29:03 +1000

    nvim(opencode): disabled for now, using directly

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -14,6 +14,7 @@ return {
   },
   {
     "NickvanDyke/opencode.nvim",
+    enabled = false,
     dependencies = {
       -- Recommended for `ask()` and `select()`.
       -- Required for default `toggle()` implementation.
