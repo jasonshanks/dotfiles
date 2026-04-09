380e16fc27beaf98422854cbfa5d97ae2a004d5c
Jason Shanks <jason.s@wellmark.com.au>
2026-04-09 11:30:18 +1000

    nvim(snacks): disabled Snacks dashboard to try native 0.12 one

--- nvim/lua/plugins/editor.snacks.lua
+++ nvim/lua/plugins/editor.snacks.lua
@@ -4,35 +4,35 @@ return {
 
     ---@type snacks.Config
     opts = {
-      dashboard = {
-        preset = {
-          header = [[
-                                        oo            
-                                                  
-88d888b. .d8888b. .d8888b. dP   .dP dP 88d8b.d8b. 
-88'  `88 88ooood8 88'  `88 88   d8' 88 88'`88'`88 
-88    88 88.  ... 88.  .88 88 .88'  88 88  88  88 
-dP    dP `88888P' `88888P' 8888P'   dP dP  dP  dP]],
-          -- stylua: ignore
-          ---@type snacks.dashboard.Item[]
-          keys = {
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-          },
-        },
-      },
-      notifier = {
-        enabled = true,
-        --- available style: "compact"|"fancy"|"minimal"
-        style = "compact",
-      },
+      --       dashboard = {
+      --         preset = {
+      --           header = [[
+      --                                         oo
+      --
+      -- 88d888b. .d8888b. .d8888b. dP   .dP dP 88d8b.d8b.
+      -- 88'  `88 88ooood8 88'  `88 88   d8' 88 88'`88'`88
+      -- 88    88 88.  ... 88.  .88 88 .88'  88 88  88  88
+      -- dP    dP `88888P' `88888P' 8888P'   dP dP  dP  dP]],
+      --           -- stylua: ignore
+      --           ---@type snacks.dashboard.Item[]
+      --           keys = {
+      --             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      --             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      --             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      --             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      --             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      --             { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      --             { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+      --             { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+      --             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+      --           },
+      --         },
+      --       },
+      -- notifier = {
+      --   enabled = true,
+      --   --- available style: "compact"|"fancy"|"minimal"
+      --   style = "compact",
+      -- },
       picker = {
         enabled = true,
         matcher = {
